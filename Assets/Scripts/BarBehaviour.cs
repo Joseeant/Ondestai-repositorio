adbab208c2d86e78b40df82593a3013c0f868bc0
Caku4 <tdronwtf@gmail.com>
2020-10-25 17:12:18 +0100

    Se añaden las barras de las stats

---
+++ Assets/Scripts/BarBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarBehaviour : MonoBehaviour
+{
+
+    private PlayerBehaviour playerBehaviour;
+    public bool moneyBar;
+    public bool alcoholBar;
+    public bool funBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBehaviour = GameObject.FindWithTag("Player").GetComponent<PlayerBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moneyBar)
+        {
+            this.GetComponent<Slider>().value = playerBehaviour.moneyStatus;
+        } else if (alcoholBar)
+        {
+            this.GetComponent<Slider>().value = playerBehaviour.alcoholStatus;
+        } else if (funBar)
+        {
+            this.GetComponent<Slider>().value = playerBehaviour.funStatus;
+        }
+    }
+}
